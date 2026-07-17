7c2ea7572a02a44dfa5a571117768d102db441ba
miguelmuete-wqxz <miguelmuetedon@gmail.com>
2026-07-17 01:37:31 -0500

    Add functions to display messages about robots, music, and books

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 
+function mostrarRobot(){
+
+document.getElementById("mensaje").innerHTML=
+"🤖 Mi pasión por crear robots nace de combinar electrónica, programación e imaginación.";
+
+}
+
+
+
+function mostrarMusica(){
+
+document.getElementById("mensaje").innerHTML=
+"🎵 La música representa emociones, creatividad y la conexión entre humanos y tecnología.";
+
+}
+
+
+
+function mostrarLibros(){
+
+document.getElementById("mensaje").innerHTML=
+"📚 Los libros son portales hacia nuevas ideas, filosofía y mundos desconocidos.";
+
+}
+
